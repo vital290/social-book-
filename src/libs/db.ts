6341020be1bd58cdba3550b1f167e431e27dc3ac
Tushar <tripathitushar67@gmail.com>
2023-05-09 23:15:01 +0530

    tusar

---
+++ src/libs/db.ts
@@ -0,0 +1,10 @@
+import { Redis } from "@upstash/redis";
+
+
+export const db = new Redis({
+
+  url:"https://eu2-enough-lemming-31535.upstash.io",
+  token:"AXsvASQgYmViMzY3OWMtNDdiNS00YzBkLWFlMmItNTY0NzAzNDc0NmIzMzMzMTM0YTU2NzA0NDBiNzhjNGYxMTkwM2Q3YTJlYmQ=",
+})
+
+
